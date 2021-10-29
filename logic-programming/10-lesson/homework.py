d1bf2f8948b87330d10ef98f0c47bf81b0045d12
BrendanGlancy <bglance68@gmail.com>
2021-10-29 14:47:46 -0400

    🙃 kind of a rough homework

--- logic-programming/10-lesson/homework.py
+++ logic-programming/10-lesson/homework.py
@@ -1,5 +1,7 @@
 # Complete Programming Exercises 1-3 on page 413.
 
+import random
+
 def main():
     total_sales()
     lottery_num()
@@ -35,7 +37,20 @@ Design a program that generates a 7-digit lottery number. The program should hav
 """
 
 def lottery_num():
-    pass
+    # init lotto_num
+    lotto_num = []
+
+    # make it look nicer
+    print("Lottery Number: ")
+
+    # step through
+    for i in range(7):
+        lotto_num.append(random.randint(0, 9))
+    for j in lotto_num:
+        print(j, end="",)
+    print("\n")
+
+    
 
 # Rainfall Stats
 """
@@ -43,7 +58,38 @@ Design a program that lets the user enter the total rainfall for each of 12 mont
 """
 
 def rainfall_stats():
-    pass
+    # init months arr and year
+    months =  [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ]
+    year = []
+    for i in months:
+        # were getting the input data in the format i+"|"
+        # will use lambda to split at |
+        year.append(i+"|"+str(input('Please enter '+i+' rainfall: ')))
+
+    # lambda parameter_list: expression
+    # min and max function returns the smallest or largest item in an
+    # iterable
+    key_func = lambda s:s.split("|")[1]
+    split_func = lambda s,index:s.split("|")[index]
+
+    # get the min rainfall
+    min_rainfall = min(year,key=key_func)
+    print("Minimum Rainfall: "+split_func(min_rainfall,1)+" in the month of "+split_func(min_rainfall,0))
+
+
+    # get the max rainfall
+    max_rainfall = max(year,key=key_func)
+    print("Max Rainfall: "+split_func(max_rainfall,1)+" in the month of "+split_func(max_rainfall,0))
+
+    # get total
+    total = sum([float(split_func(p,1)) for p in year])
+    print("Total Rainfall: "+str(total))
+
+    # avg = total / len(year)
+    print("Average Rainfall: "+str(float(total/len(year))))
+
+
+
 
 if __name__ == "__main__":
     main()

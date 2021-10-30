846e883bdf0f6c74997e916a075df691cf8c5b03
BrendanGlancy <bglance68@gmail.com>
2021-10-29 23:54:00 -0400

    cleanup

--- test.py
+++
@@ -1,9 +0,0 @@
-speed = 10
-miles = 5
-print(speed + 12 - miles * 2)
-print(speed + miles * 3)
-print((speed + miles) * 3)
-print(speed + speed * miles + miles)
-print((10 - speed) + miles / miles)
-a = 0
-a = a

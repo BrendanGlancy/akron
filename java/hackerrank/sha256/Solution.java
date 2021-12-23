0259c95f09c83926227a47f0f24aadb00c3c1478
brendanglancy <bglance68@gmail.com>
2021-12-23 00:19:02 -0500

    working

--- java/hackerrank/sha256/Solution.java
+++ java/hackerrank/sha256/Solution.java
@@ -7,16 +7,18 @@ public class Solution {
     public static void main(String[] args) throws NoSuchAlgorithmException {
 
         Scanner input = new Scanner(System.in);
-        /* Enter your code here. Print output to STDOUT. */
-        MessageDigest m = MessageDigest.getInstance("SHA-256");
-        m.reset();
-        m.update(input.nextLine().getBytes());
+        String str = input.next();
+        /*
+         * Enter your code here. Print output to STDOUT.
+         * Works for both MD5 and SHA-256 problems
+         */
+        MessageDigest m = MessageDigest.getInstance("MD5");
+        m.update(str.getBytes());
         for (byte i : m.digest()) {
-            System.out.print(String.format("02x", i));
+            System.out.print(String.format("%02x", i));
         }
         System.out.println();
         input.close();
 
     }
 }
-

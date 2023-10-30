50512badb5add731859d15687750b502914836b3
brendanglancy <bglance68@gmail.com>
2023-10-29 21:18:31 -0400

    update this

---
+++ tmp/copier.py
@@ -0,0 +1,33 @@
+import pyperclip
+import time
+import os
+import openai
+
+def clipboard_listener():
+    last_clipboard_value = ""
+    
+    while True:
+        clipboard_value = pyperclip.paste()
+        if clipboard_value != last_clipboard_value:
+            last_clipboard_value = clipboard_value
+            chat_gepity(clipboard_value)
+        time.sleep(1)
+
+
+def chat_gepity(clipboard_value):
+    print(f"Copied to clipboard: {clipboard_value}")
+    openai.organization = "org-CUdodZ48VjZcXBFRtTrlRuFw"
+    openai.api_key = ""
+    # print(openai.Model.list())
+    
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Answer this multiple choice or fill in the blank question as an expert on system design and analysis: " + clipboard_value,
+        max_tokens=100
+    )
+    
+    answer = response.choices[0].text.strip()
+    print(f"Answer from API: {answer}")
+
+if __name__ == "__main__":
+    clipboard_listener()

8c477b2d61006149e02358879abf7f470144be99
BrendanGlancy <bglance68@gmail.com>
2024-01-29 11:53:13 -0500

    untracked file for cpp

---
+++ cpp/assignment-1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+/*
+  Project Requirement:
+  For a single table [struct] and UI, do data entry and write each record
+  Example: "patient demographic record"
+  - Subject to me finding the right HL7 or EMR nomenclature
+  - SSN, building an alpha string from digit entries
+  - Name, alpha firstname and lastname and character middle initial
+  - Address: two alphabetic for address, one for city
+  - State code, which we will populate and validate later
+  - ZIP, entered as digits and then concatenated as either number or string, TBD
+  - I expect reasonable validation for each field; "minimal" style
+  - OK to do this twice: once with functions in main(), also
+*/
+
+int main() {}
